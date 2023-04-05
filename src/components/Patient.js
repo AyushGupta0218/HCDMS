9e2ca7bfe950f90d5e4f73e7f52fefdc934bb350
AyushGupta0218 <ayush.gupta0218@gmail.com>
2023-04-05 20:37:19 +0530

    initial commit

---
+++ src/components/Patient.js
@@ -0,0 +1,89 @@
+import React from 'react'
+
+export default function Patient() {
+  return (
+        <form>
+
+        <div className="form-row">
+            <div className="form-group col-md-6">
+            <label for="inputFirstName">First Name</label>
+            <input type="text" className="form-control" id="inputFirstName" placeholder="Enter First Name"/>
+            </div>
+            <div className="form-group col-md-6">
+            <label for="inputLastName">Last Name</label>
+            <input type="text" className="form-control" id="inputLaseName" placeholder="Enter Last Name"/>
+            </div>
+        </div>
+
+        <div className="form-row">
+            <div className="form-group col-md-6">
+            <label for="inputFirstName">Date of birth</label><br/>
+             
+
+                 <input type="date" id="start" name="trip-start" value="2022-07-22" min="1975-01-01" max="2022-04-09"/>
+            </div>
+            <div className="form-group col-md-6">
+            <label for="inputState">Gender</label>
+            <select id="inputState" className="form-control">
+                <option selected>Choose</option>
+                <option value="1">Male</option>
+            <option value="2">Female</option>
+            </select>
+
+            
+
+            {/* <input type="text" className="form-control" id="inputLaseName" placeholder="Enter Last Name"/> */}
+            </div>
+        </div>
+
+        <div className="form-row">
+            <div className="form-group col-md-6">
+            <label for="inputEmail4">Email</label>
+            <input type="email" className="form-control" id="inputEmail4" placeholder="Email" value ={name} onChange={(e) => e.target.value}/>
+            </div>
+            <div className="form-group col-md-6">
+            <label for="inputPassword4">Password</label>
+            <input type="password" className="form-control" id="inputPassword4" placeholder="Password"/>
+            </div>
+        </div>
+        <div className="form-group">
+            <label for="inputAddress">Address</label>
+            <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"/>
+        </div>
+        <div className="form-group">
+            <label for="inputAddress2">Address 2</label>
+            <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
+        </div>
+        <div className="form-row">
+            <div className="form-group col-md-6">
+            <label for="inputCity">City</label>
+            <input type="text" className="form-control" id="inputCity"/>
+            </div>
+            <div className="form-group col-md-4">
+            <label for="inputState">State</label>
+            <select id="inputState" className="form-control">
+                <option selected>Choose...</option>
+                <option>...</option>
+            </select>
+            </div>
+            <div className="form-group col-md-2">
+            <label for="inputZip">Zip</label>
+            <input type="text" className="form-control" id="inputZip"/>
+            </div>
+        </div>
+        <div className="form-group">
+            <div className="form-check">
+            <input className="form-check-input" type="checkbox" id="gridCheck"/>
+            <label className="form-check-label" for="gridCheck">
+                Check me out
+            </label>
+            </div>
+        </div>
+
+        <div>
+        
+        </div>
+        <button type="submit" className="btn btn-primary">Sign in</button>
+        </form>
+  )
+}
